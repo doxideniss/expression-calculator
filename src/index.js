ac46e0762b2ce907cabb0d72233a055c7420634e
Deniss <doxideniss@gmail.com>
2020-03-06 22:34:33 +0200

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+    expr = expr.replace(/ /g, '')
+
+    const bracketOn = expr.match(/\(/g) === null ? 0 : expr.match(/\(/g).length;
+    const bracketOff = expr.match(/\)/g) === null ? 0 : expr.match(/\)/g).length;
+
+    const isPairBrackets = bracketOn === bracketOff
+
+    if (!isPairBrackets) {
+        throw new Error("ExpressionError: Brackets must be paired")
+    }
+
+    if ( expr.includes("/0") ) {
+        throw new Error("TypeError: Division by zero.");
+    }
+
+    return new Function("return " + expr)()
+
 }
 
 module.exports = {
